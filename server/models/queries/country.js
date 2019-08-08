e646e870d446710a2987c9e6670308512ec0e581
GeoAstronaute <jeanmarc.royy@gmail.com>
2019-08-08 08:30:03 +0200

    (Server) add list query to country entity

--- server/models/queries/country.js
+++ server/models/queries/country.js
@@ -1,12 +1,24 @@
+var request = require('request');
+
 class CountryModel {
+
 	constructor(app, entity) {
 		this.app = app;
 		this.entity = entity;
 	}
 
-	azdazd(params) {
-		// TODO
+	list(params) {
+		return new Promise((resolve, reject) => {
+            request({url: 'https://restcountries.eu/rest/v2/all', json: true}, (err, response, body) => {
+				if (err) {
+					return reject(err);
+				}
+				resolve(body);
+			})
+
+		});
 	}
+	
 }
 
 module.exports = CountryModel;
